975ea3026461b904d01879027cc138ed77caba28
Kartik <realmenarzo53@gmail.com>
2026-04-11 18:33:28 +0000

    compare them

---
+++ 01_basics/comparison.js
@@ -0,0 +1,7 @@
+//  console.log("2">1);
+//  console.log("02">1);
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null>=0)
+// reason is => // conversion converts null into a number treating it as 0 thats why for (1) it is false and for (3) it is true;
