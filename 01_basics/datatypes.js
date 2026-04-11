11b9dbdaea85bae10d1837aae9a7aeefb5721f5c
Kartik <realmenarzo53@gmail.com>
2026-04-11 10:10:44 +0000

    datatypes and ecma

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+ "use strict" ;// treat all js node as newer version of js
+ console.log("3+3");
+ let name = "brad pitt"
+ let age = 45
+ let isLoggedIn = false
+
+ /* number : 2 to the power 53
+     bigInt
+     string : " "
+     boolean : trueor false
+     null : standalone value
+     undefined : 
+     symbol : unique
+     object
+*/
+console.log(typeof"brad pitt");
+console.log(typeof age );
+console.log(typeof null );
+console.log(typeof undefined);
