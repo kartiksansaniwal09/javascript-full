f89385ec86b3c337f5145f8222549413acb8bd46
Kartik <realmenarzo53@gmail.com>
2026-04-11 17:09:13 +0000

    conversion of datatypes

---
+++ 01_basics/conversionnOperations.js
@@ -0,0 +1,29 @@
+//  let score = 33
+//  console.log(typeof score);
+ // now //
+//  let score  = "33abc"
+//  let valueInNumber = Number(score)
+//  console.log(score);
+//  console.log(typeof score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+ 
+ /* learning and notes :=>
+    "33" => converts to number easily
+ but "33abc" => gives NaN  means not a number;
+ true => 1
+ flase => 0
+*/
+let isLoggedIn = ""
+let booleanIsLOoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLOoggedIn);
+/*
+1 => true; 0 => flase;
+"" => false
+"jackma" => true 
+
+*/
+let aNumber = 44
+let stringNumber = String(aNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
