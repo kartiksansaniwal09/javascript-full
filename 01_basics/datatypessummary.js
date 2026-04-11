61b1836c373bee703bc077e31125804a9b45ab0c
Kartik <realmenarzo53@gmail.com>
2026-04-11 19:09:08 +0000

    more insights

--- 01_basics/datatypessummary.js
+++ 01_basics/datatypessummary.js
@@ -12,3 +12,15 @@
 //        Arrays  =>  object
 //        Function  =>  function
 //        Object  =>  object
+
+
+
+//************* stack memory and heap memory ******************************** */
+// stack is for primitive and heap is for non primitive //
+// stack me copy milta hai aur heap me refrence milta hai //
+let myYoutubeName = "mylifemyrules"
+let anotherName =  myYoutubeName
+anotherName = "iamliving"
+console.log(anotherName);
+console.log(myYoutubeName);
+// this is stack mem //
